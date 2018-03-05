e82c241b36e5faf1a801a8856a8e56a91f2b2540
Wilfrid Utz <wilfrid.utz@boc-eu.com>
2018-03-05 22:23:36 +0100

    Changed implementation from webrunner to embedded

---
+++ src/main/java/org/mymess/weblogger/appender/EmbeddedTomcatRunner.java
@@ -0,0 +1,27 @@
+package org.mymess.weblogger.appender;
+
+import java.io.File;
+
+import javax.servlet.ServletException;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class EmbeddedTomcatRunner {
+
+    public static void main(String[] args) throws ServletException, LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        // The port that we should run on can be set into an environment variable
+        // Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        tomcat.setPort(Integer.valueOf(webPort));
+        String webappDirLocation = "src/main/webapp/";
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+}
